2d93cf663d996219dd588ed90cc4f4b4c0e4d445
csabagyorfi <csaba@madetech.com>
2022-02-14 20:12:00 +0000

    Output day1 result with description

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "day_one_part_one_result" {
+  description = <<EOF
+    Numbwe of times the value increased comparing to the previous value,
+    more details: https://adventofcode.com/2021/day/1
+  EOF
+  value = local.result
+}
